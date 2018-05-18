d1a868cc071de3989b1230d9b2f1896979b55dd9
centsuse <903591127@qq.com>
2018-05-18 14:03:29 +0800

    init

---
+++ src/main/java/com/centsuse/array/test.java
@@ -0,0 +1,14 @@
+package com.centsuse.array;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author bobo
+ * @create 2018-05-18 下午2:01
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+        System.out.println("now, let's do this!!!");
+    }
+}
